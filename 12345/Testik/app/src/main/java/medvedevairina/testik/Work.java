9139c96dd9077954f1fe0c15adb77ac6e6644f9b
Хрюндик <kembel.n@mail.ru>
2016-01-22 19:29:23 +0500

    list

---
+++ 12345/Testik/app/src/main/java/medvedevairina/testik/Work.java
@@ -0,0 +1,20 @@
+package medvedevairina.testik;
+
+public class Work {
+    private String name;
+    private String author;
+
+    public Work(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+
